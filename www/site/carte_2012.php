6eebe90bca564e17cfc875a9fe87c3999d956cd8
LamaDelRay <salvetti3@live.fr>
2014-04-26 15:41:41 +0200

    Force de travail!

--- www/site/carte_2012.php
+++ www/site/carte_2012.php
@@ -75,7 +75,7 @@
   
   <div class="collapse navbar-collapse navbar-ex1-collapse">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="carte_2009.php">Carte 2009</a></li>
+      <li class="active"><a href="carte_2012.php">Carte 2012</a></li>
       <li><a href="index.php">Accueil</a></li>
       <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Cartes <span class="caret"></span></a>
@@ -113,9 +113,9 @@
     <div class="col-md-6 col-md-offset-3">
  
       <div class="caption">
-        <h3>Région île-de-France : 2009</h3>
+        <h3>Région île-de-France : 2012</h3>
         <p>
-          Le graphique représente les infractions commises dans la région île-de-France par département en 2009. Ces données ont été recueillies par la Police et la Gendarmerie Nationale et ont été publiés dans les journaux de la République ainsi que sur justice.gouv.fr et data.gouv.fr .
+          Le graphique représente les infractions commises dans la région île-de-France par département en 2012. Ces données ont été recueillies par la Police et la Gendarmerie Nationale et ont été publiés dans les journaux de la République ainsi que sur justice.gouv.fr et data.gouv.fr .
         </p>
         
       </div>
@@ -130,14 +130,14 @@
         <map name="Map">
         
         
-          <area id="jquerytest" shape="poly" coords="134,242,124,242,107,224,106,206,72,165,80,153,70,140,79,126,51,76,56,68,49,61,78,51,98,64,119,58,131,76,141,66,148,76,174,79,196,107,186,111,180,132,192,154,169,157,155,186,162,194,154,210,144,210,147,222" href="?bouton=y2009&envoi=Yvelines+2009#">
-          <area shape="poly" coords="78,51,91,33,92,21,106,7,111,14,104,23,123,33,184,25,187,34,208,40,215,32,261,51,270,63,260,73,247,95,207,96,196,107,174,80,149,78,142,66,132,75,120,59,99,63" href="?bouton=vo2009&envoi=Val+d%27Oise+2009#">
-          <area shape="poly" coords="270,62,262,74,272,103,263,119,268,130,267,142,271,154,260,179,246,241,251,257,223,284,244,306,247,323,233,336,280,342,291,331,298,338,327,337,338,324,347,305,341,296,350,279,414,272,424,230,433,232,435,213,450,200,430,202,436,168,426,158,436,135,375,82,380,69,374,61,320,64,309,58,290,67,280,59" href="?bouton=sm2009&envoi=Seine+et+Marne+2009#">
-          <area shape="poly" coords="221,283,193,285,182,271,175,283,142,285,143,258,132,253,133,243,148,223,143,211,153,209,162,194,155,185,169,158,191,156,210,161,224,169,247,166,260,179,245,240,250,256" href="?bouton=e2009&envoi=Essonne+2009#">
-          <area shape="poly" coords="212,161,217,136,229,136,235,130,250,130,268,140,271,154,260,179,248,165,224,169" href="?bouton=vm2009&envoi=Val+de+Marne+2009#">
-          <area shape="poly" coords="261,75,271,100,264,120,269,130,267,140,249,130,236,130,225,118,216,116,217,103,205,98,245,95" href="?bouton=sd2009&envoi=Seine-Saint-Denis+2009#">
-          <area shape="poly" coords="212,161,192,155,180,132,187,111,195,109,207,99,217,106,215,117,200,122,199,130,216,138" href="?bouton=h2009&envoi=Hauts+de+Seine+2009#">
-          <area shape="poly" coords="216,118,225,119,236,128,231,136,218,138,201,130,201,123" href="?bouton=p2009&envoi=Paris+2009#">
+          <area id="jquerytest" shape="poly" coords="134,242,124,242,107,224,106,206,72,165,80,153,70,140,79,126,51,76,56,68,49,61,78,51,98,64,119,58,131,76,141,66,148,76,174,79,196,107,186,111,180,132,192,154,169,157,155,186,162,194,154,210,144,210,147,222" href="?bouton=y2012&envoi=Yvelines+2012#">
+          <area shape="poly" coords="78,51,91,33,92,21,106,7,111,14,104,23,123,33,184,25,187,34,208,40,215,32,261,51,270,63,260,73,247,95,207,96,196,107,174,80,149,78,142,66,132,75,120,59,99,63" href="?bouton=vo2012&envoi=Val+d%27Oise+2012#">
+          <area shape="poly" coords="270,62,262,74,272,103,263,119,268,130,267,142,271,154,260,179,246,241,251,257,223,284,244,306,247,323,233,336,280,342,291,331,298,338,327,337,338,324,347,305,341,296,350,279,414,272,424,230,433,232,435,213,450,200,430,202,436,168,426,158,436,135,375,82,380,69,374,61,320,64,309,58,290,67,280,59" href="?bouton=sm2012&envoi=Seine+et+Marne+2012#">
+          <area shape="poly" coords="221,283,193,285,182,271,175,283,142,285,143,258,132,253,133,243,148,223,143,211,153,209,162,194,155,185,169,158,191,156,210,161,224,169,247,166,260,179,245,240,250,256" href="?bouton=e2012&envoi=Essonne+2012#">
+          <area shape="poly" coords="212,161,217,136,229,136,235,130,250,130,268,140,271,154,260,179,248,165,224,169" href="?bouton=vm2012&envoi=Val+de+Marne+2012#">
+          <area shape="poly" coords="261,75,271,100,264,120,269,130,267,140,249,130,236,130,225,118,216,116,217,103,205,98,245,95" href="?bouton=sd2012&envoi=Seine-Saint-Denis+2012#">
+          <area shape="poly" coords="212,161,192,155,180,132,187,111,195,109,207,99,217,106,215,117,200,122,199,130,216,138" href="?bouton=h2012&envoi=Hauts+de+Seine+2012#">
+          <area shape="poly" coords="216,118,225,119,236,128,231,136,218,138,201,130,201,123" href="?bouton=p2012&envoi=Paris+2012#">
         </map>
     </div>
     </div>
